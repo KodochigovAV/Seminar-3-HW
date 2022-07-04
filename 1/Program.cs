fa08e97908e826e674dc25637aabdae6f9de3b1d
Kodochigov Aleksandr <kodochigov.aleksandr@icloud.com>
2022-07-04 19:19:57 +0300

    init comm

---
+++ 1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Напишите программу, которая принимает 
+// на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+
+int Num = Prompt("Enter five-digit -> ");
+if (Num <= 9999 || Num > 99999)
+{
+    System.Console.WriteLine("Error");
+    Environment.Exit(0);
+}
+
+if (Num / 10000 == Num % 10 && Num / 1000 % 10 == Num % 100 / 10) System.Console.WriteLine("OK");
+else System.Console.WriteLine("NO");
---
+++ 3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход число (N) и
+// выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27. 
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+
+int N = Prompt("Enter -> ");
+
+int i = 1;
+
+while(i <= N)
+{
+    System.Console.WriteLine(i*i*i);
+    i++;
+}
